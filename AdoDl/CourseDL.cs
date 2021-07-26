bc9dd49fb0eaffe0988358dfe6cbac673cacd167
Mayank Awasthi <mayank.awasthi@ltts.com>
2021-07-26 20:31:09 +0530

    first commit

---
+++ AdoDl/CourseDL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AdoDl
+{
+    public class CourseDL
+        {
+            SqlConnection sqlConObj = null;
+            SqlCommand sqlCmdObj = null;
+            public void ConnectToDB()
+            {
+                try
+                {
+                    sqlConObj = new SqlConnection(ConfigurationManager.ConnectionStrings["Crs"].ToString());
+                    sqlConObj.Open();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something Happened!");
+                }
+                finally
+                {
+                    sqlConObj.Close();
+                }
+            }
+
+            public List<String> ReadfromDB()
+            {
+                List<String> lstCourses = new List<string>();
+                try
+                {
+                    sqlConObj = new SqlConnection(ConfigurationManager.ConnectionStrings["Crs"].ToString());
+                    sqlCmdObj = new SqlCommand(@"Select * from Courses", sqlConObj);
+                    sqlConObj.Open();
+                    SqlDataReader drCourses = sqlCmdObj.ExecuteReader();
+                    while (drCourses.Read())
+                    {
+                        lstCourses.Add(String.Concat(drCourses["CourseId"] + "  ", drCourses["CourseTitle"] + "  ", drCourses["Duration"] + "  ", drCourses["Owner"] + "  ", drCourses["AssessmentMode"]));
+                    }
+                    return lstCourses;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something Happened!");
+                    return lstCourses;
+                }
+                finally
+                {
+                    sqlConObj.Close();
+                }
+            }
+        }
+}
+
+
